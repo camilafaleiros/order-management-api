2d424c1792325d3a48142cc8feaf74c6ce63740b
camilafaleiros <93732809+camilafaleiros@users.noreply.github.com>
2025-09-26 17:20:46 -0300

    Update HealthCheckController.java

--- src/main/java/com/example/api_pedidos/controller/HealthCheckController.java
+++ src/main/java/com/example/api_pedidos/controller/HealthCheckController.java
@@ -3,10 +3,9 @@ package com.example.api_pedidos.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController  // diz pro Spring que essa classe é um controller REST
+@RestController  
 public class HealthCheckController {
 
-    // esse metodo vai responder quando você acessar /ping
     @GetMapping("/ping")
     public String ping() {
         return "pong";
